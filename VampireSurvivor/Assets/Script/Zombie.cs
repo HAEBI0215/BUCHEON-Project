8a2dd071b5bee9b0c9971ed72c1de610d66354e0
HAEBI0215 <haebi0215@gmail.com>
2026-01-16 15:12:34 +0900

    2일차 3

---
+++ VampireSurvivor/Assets/Script/Zombie.cs
@@ -0,0 +1,130 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Zombie : MonoBehaviour
+{
+    public enum LiveState
+    {
+        Live,
+        Dead
+    }
+    public LiveState liveState = LiveState.Live;
+
+    public enum ActionState
+    {
+        Idle,
+        Move,
+        Attack
+    }
+    public ActionState actionState = ActionState.Idle;
+
+    private GameObject player;
+    private NavMeshAgent agent;
+    public Animator anim;
+
+    public float attackRange;
+    public enum AttackState
+    {
+        None,
+        Attack,
+        Delay
+    }
+    public AttackState attackState = AttackState.None;
+    private float attackTime;
+    private float delayTime;
+    public AnimationClip attackClip;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        //공격 반경만큼 와이어 구체를 그림
+    }
+
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        Action();
+    }
+
+    void Action()
+    {
+        switch (actionState)
+        {
+            case ActionState.Idle:
+                {
+                    if (player)
+                    {
+                        agent.isStopped = false;
+                        AnimOn(1);
+                        actionState = ActionState.Move;
+                    }
+                    break;
+                }
+            case ActionState.Move:
+                { 
+                    if (player)
+                    {
+                        agent.SetDestination(player.transform.position);
+                        //플레이어를 향해 이동함
+
+                        float dist = Vector3.Distance(transform.position, player.transform.position);
+                        //좀비와 플레이어 사이의 거리를 float형으로 반환
+
+                        if (dist <= attackRange)
+                        {
+                            agent.isStopped = true;
+                            AnimOn(2);
+                            attackState = AttackState.Attack;
+                            actionState = ActionState.Attack;
+                        }
+                    }
+                    break;
+                }
+            case ActionState.Attack:
+                {
+                    switch (attackState)
+                    {
+                        case AttackState.Attack:
+                            {
+                                attackTime += Time.deltaTime;
+                                if (attackTime >= attackClip.length * 0.25f)
+                                {
+                                    AnimOn(0);
+                                    attackTime = 0;
+                                    delayTime = 0;
+                                    attackState = AttackState.Delay;
+                                }
+
+                                break;
+                            }
+                        case AttackState.Delay:
+                            {
+                                delayTime += Time.deltaTime;
+                                if (delayTime >= attackClip.length * 0.75f)
+                                {
+                                    AnimOn(2);
+                                    attackTime = 0;
+                                    delayTime = 0;
+                                    attackState = AttackState.Attack;
+                                }
+                                break;
+                            }
+                    }
+                    break;
+                }
+        }
+    }
+
+    void AnimOn(int n)
+    {
+        anim.SetInteger("ZombieState", n);
+    }
+}
